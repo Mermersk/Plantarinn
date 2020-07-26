35379b564b3b191b66a66eaabb84fde4aa9617f1
Mermersk <ic4ruz39@gmail.com>
2020-07-26 23:53:53 +0100

    New sound effect for each plantInterval. Changing some types. Started on infoboxes in Planting Activity screen.

--- src/main/java/com/example/plantarinn/PlantingActivity.kt
+++ src/main/java/com/example/plantarinn/PlantingActivity.kt
@@ -1,12 +1,15 @@
 package com.example.plantarinn
 
+import kotlin.math.round
 import android.content.Context
+import android.graphics.Color
 import android.media.MediaPlayer
 import android.media.SoundPool
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.OnLifecycleEvent
@@ -22,34 +25,38 @@ class PlantingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_planting)
 
         val pp: PlantPlan? = intent.getParcelableExtra(EXTRA_PLANTING)
-        if (pp != null) {
-            //println("Coming from PlantingActivity ${pp.totalTimeS}")
-        }
 
         val plantingGraphics = findViewById<PlantingMainGraphics>(R.id.plantingMainGraphics)
         //converting plantInterval from seconds til milliseconds
         val plantIntervalms = (pp?.plantInterval?.times(1000))
         //Using elvis operator for pp.plantInterval, if plantInterval is null then pass in 1.0
-        plantingGraphics.setAnimationDuration(plantIntervalms!!)
+        plantingGraphics.setAnimationDuration(plantIntervalms!!.toLong())
         plantingGraphics.startAnimation()
+        playSound(plantIntervalms.toLong())
 
-        playSound(plantIntervalms)
+        val plantSpeedInfo = findViewById<TextView>(R.id.plantSpeedInfo)
+        plantSpeedInfo.text = "Time each plant: ${pp.plantInterval}s"
+        plantSpeedInfo.setBackgroundColor(Color.LTGRAY)
+
+        val totalPlantingTimeInfo = findViewById<TextView>(R.id.totalPlantingTimeInfo)
+        totalPlantingTimeInfo.text = "${pp.hours} hours & ${pp.minutes} minutes"
+        totalPlantingTimeInfo.setBackgroundColor(Color.LTGRAY)
 
 
     }
-
-    //called when play button is pressed
+    
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     fun playSound(period: Long) {
 
         //Initializing SoundPool for playing audio
         val sp = SoundPool.Builder()
         sp.setMaxStreams(1)
         val readySP = sp.build()
-        val oneLoopCompleteSoundID = readySP.load(this, R.raw.duck1, 1)
+        val oneLoopCompleteSoundID = readySP.load(this, R.raw.blipblop2, 1)
 
 
         //extends TimerTask. Runs on its own thread. Feed This object to Timer.schedule function
-        class playSoundTask() : TimerTask() {
+        class PlaySoundTask() : TimerTask() {
 
             override fun run() {
                 println("Hopefully gonn aplay sound soon")
@@ -58,7 +65,7 @@ class PlantingActivity : AppCompatActivity() {
         }
 
         //Creating a PlaySoundTask
-        val pst = playSoundTask()
+        val pst = PlaySoundTask()
 
         //Scheduling the timer to execute run() inside of PlaySoundTask every period.
         //period here in this case is the plantInterval value that was calculated in PlantPlan
